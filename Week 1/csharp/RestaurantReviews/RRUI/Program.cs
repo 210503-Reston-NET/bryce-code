8f020258551f88ddc616aff04b1ec2d9c8d0cdab
Bryce Zimbelman <bryce.zimbelman@snhu.edu>
2021-05-04 12:54:23 -0500

    Added UI

---
+++ Week 1/csharp/RestaurantReviews/RRUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RRModels;
+
+namespace RRUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Restarant goodTaste = new Restarant("Good Taste", "Baguio City", "Benguet");
+            goodTaste.Review = new Review 
+            {
+                rating = 5,
+                Description = "A M A Z I N G"
+            };
+            Console.WriteLine(goodTaste.ToString());
+        }
+    }
+}
